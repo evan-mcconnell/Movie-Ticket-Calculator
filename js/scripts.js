4f26a59cff15904751e5352702b7e6487b989762
Kimberly McConnell and Richard Laws <richard@>
2018-10-15 20:12:38 -0700

    add fcuntionality to calculate a ticket price and display for the user with their name

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,41 @@ function Ticket(name, age, film, showtime) {
   this.film = film;
   this.showtime = showtime;
 }
+var price = 10
+
+function priceCalc(x) {
+  if (x.age <= 10 || x.age >= 65) {
+    price = price - 2;
+    console.log(price + "age");
+  }
+  if (x.film === 2) {
+    price = price - 2;
+    console.log(price);
+  }
+  if (x.showtime === 1) {
+    price = price - 1;
+    console.log(price);
+  }
+  return price
+};
 
 //UI Logic
 $(document).ready(function() {
   $("#ticket-form").submit(function(event) {
     event.preventDefault();
+    price = 10;
     var name = $("#name").val();
-    var age = $("#age").val();
+    var age = parseInt($("#age").val());
     var film = $("#film").val();
     var showtime = $("#showtime").val();
 
-    console.log(name);
-    var newTicket = new Ticket()
+    console.log(film);
+    // debugger;
+    var newTicket = new Ticket(name, age, film, showtime)
+    var newPrice = priceCalc(newTicket);
+    console.log(newTicket);
+    // console.log(newPrice);
+    $("span#outputs").text("Hi " + name + ", your ticket price is $" + newPrice );
 
   });
 });
